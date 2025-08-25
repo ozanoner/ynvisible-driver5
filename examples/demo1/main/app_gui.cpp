5e66e0c451ebd89f5f74a10d9e1f9092a8f67e19
Ozan Oner <ozan.oner@gmail.com>
2025-08-25 14:26:52 +0000

    Add anim buttons

--- examples/demo1/main/app_gui.cpp
+++ examples/demo1/main/app_gui.cpp
@@ -29,52 +29,103 @@ esp_err_t Gui::show()
 
     lv_obj_t* tv = lv_tileview_create(lv_screen_active());
 
-    lv_obj_t* tile1 = lv_tileview_add_tile(tv, 0, evalkit::DISP431V2PV1, LV_DIR_BOTTOM);
+    // Create tile for DISP431V2PV1
+    lv_obj_t* tile1 = lv_tileview_add_tile(tv, 0, evalkit::DisplayInfo::DISP431V2PV1,
+                                           static_cast<lv_dir_t>(LV_DIR_BOTTOM | LV_DIR_RIGHT));
     lv_obj_t* img1  = lv_image_create(tile1);
-    lv_image_set_src(img1, DISP_FILE_MAP.at(evalkit::DISP431V2PV1));
+    lv_image_set_src(img1, DISP_FILE_MAP.at(evalkit::DisplayInfo::DISP431V2PV1));
     lv_obj_center(img1);
+    // add animation buttons
+    addAnimationButtons(tv, evalkit::DisplayInfo::DISP431V2PV1);
 
-    tile1 = lv_tileview_add_tile(tv, 0, evalkit::DISP433V1PV1, LV_DIR_VER);
+    // Create tile for DISP433V1PV1
+    tile1 = lv_tileview_add_tile(tv, 0, evalkit::DisplayInfo::DISP433V1PV1,
+                                 static_cast<lv_dir_t>(LV_DIR_VER | LV_DIR_RIGHT));
     img1  = lv_image_create(tile1);
-    lv_image_set_src(img1, DISP_FILE_MAP.at(evalkit::DISP433V1PV1));
+    lv_image_set_src(img1, DISP_FILE_MAP.at(evalkit::DisplayInfo::DISP433V1PV1));
     lv_obj_center(img1);
+    // add animation buttons
+    addAnimationButtons(tv, evalkit::DisplayInfo::DISP433V1PV1);
 
-    tile1 = lv_tileview_add_tile(tv, 0, evalkit::DISP434V1PV1, LV_DIR_VER);
+    // create tile for DISP434V1PV1
+    tile1 = lv_tileview_add_tile(tv, 0, evalkit::DisplayInfo::DISP434V1PV1,
+                                 static_cast<lv_dir_t>(LV_DIR_VER | LV_DIR_RIGHT));
     img1  = lv_image_create(tile1);
-    lv_image_set_src(img1, DISP_FILE_MAP.at(evalkit::DISP434V1PV1));
+    lv_image_set_src(img1, DISP_FILE_MAP.at(evalkit::DisplayInfo::DISP434V1PV1));
     lv_obj_center(img1);
+    // add animation buttons
+    addAnimationButtons(tv, evalkit::DisplayInfo::DISP434V1PV1);
 
-    tile1 = lv_tileview_add_tile(tv, 0, evalkit::DISP437V2PV1, LV_DIR_VER);
+    // create tile for DISP437V2PV1
+    tile1 = lv_tileview_add_tile(tv, 0, evalkit::DisplayInfo::DISP437V2PV1,
+                                 static_cast<lv_dir_t>(LV_DIR_VER | LV_DIR_RIGHT));
     img1  = lv_image_create(tile1);
-    lv_image_set_src(img1, DISP_FILE_MAP.at(evalkit::DISP437V2PV1));
+    lv_image_set_src(img1, DISP_FILE_MAP.at(evalkit::DisplayInfo::DISP437V2PV1));
     lv_obj_center(img1);
+    // add animation buttons
+    addAnimationButtons(tv, evalkit::DisplayInfo::DISP437V2PV1);
 
-    tile1 = lv_tileview_add_tile(tv, 0, evalkit::DISP438V2PV1, LV_DIR_VER);
+    // create tile for DISP438V2PV1
+    tile1 = lv_tileview_add_tile(tv, 0, evalkit::DisplayInfo::DISP438V2PV1,
+                                 static_cast<lv_dir_t>(LV_DIR_VER | LV_DIR_RIGHT));
     img1  = lv_image_create(tile1);
-    lv_image_set_src(img1, DISP_FILE_MAP.at(evalkit::DISP438V2PV1));
+    lv_image_set_src(img1, DISP_FILE_MAP.at(evalkit::DisplayInfo::DISP438V2PV1));
     lv_obj_center(img1);
+    // add animation buttons
+    addAnimationButtons(tv, evalkit::DisplayInfo::DISP438V2PV1);
 
-    tile1 = lv_tileview_add_tile(tv, 0, evalkit::DISP440V2PV1, LV_DIR_VER);
+    // create tile for DISP440V2PV1
+    tile1 = lv_tileview_add_tile(tv, 0, evalkit::DisplayInfo::DISP440V2PV1,
+                                 static_cast<lv_dir_t>(LV_DIR_VER | LV_DIR_RIGHT));
     img1  = lv_image_create(tile1);
-    lv_image_set_src(img1, DISP_FILE_MAP.at(evalkit::DISP440V2PV1));
+    lv_image_set_src(img1, DISP_FILE_MAP.at(evalkit::DisplayInfo::DISP440V2PV1));
     lv_obj_center(img1);
+    // add animation buttons
+    addAnimationButtons(tv, evalkit::DisplayInfo::DISP440V2PV1);
 
-    tile1 = lv_tileview_add_tile(tv, 0, evalkit::DISP442V2PV1, LV_DIR_VER);
+    // create tile for DISP442V2PV1
+    tile1 = lv_tileview_add_tile(tv, 0, evalkit::DisplayInfo::DISP442V2PV1,
+                                 static_cast<lv_dir_t>(LV_DIR_VER | LV_DIR_RIGHT));
     img1  = lv_image_create(tile1);
-    lv_image_set_src(img1, DISP_FILE_MAP.at(evalkit::DISP442V2PV1));
+    lv_image_set_src(img1, DISP_FILE_MAP.at(evalkit::DisplayInfo::DISP442V2PV1));
     lv_obj_center(img1);
+    // add animation buttons
+    addAnimationButtons(tv, evalkit::DisplayInfo::DISP442V2PV1);
 
-    tile1 = lv_tileview_add_tile(tv, 0, evalkit::DISP443V2PV1, LV_DIR_VER);
+    // create tile for DISP443V2PV1
+    tile1 = lv_tileview_add_tile(tv, 0, evalkit::DisplayInfo::DISP443V2PV1,
+                                 static_cast<lv_dir_t>(LV_DIR_VER | LV_DIR_RIGHT));
     img1  = lv_image_create(tile1);
-    lv_image_set_src(img1, DISP_FILE_MAP.at(evalkit::DISP443V2PV1));
+    lv_image_set_src(img1, DISP_FILE_MAP.at(evalkit::DisplayInfo::DISP443V2PV1));
     lv_obj_center(img1);
+    // add animation buttons
+    addAnimationButtons(tv, evalkit::DisplayInfo::DISP443V2PV1);
 
-    tile1 = lv_tileview_add_tile(tv, 0, evalkit::DISP444V1PV2, LV_DIR_TOP);
+    // create tile for DISP444V1PV2
+    tile1 = lv_tileview_add_tile(tv, 0, evalkit::DisplayInfo::DISP444V1PV2,
+                                 static_cast<lv_dir_t>(LV_DIR_TOP | LV_DIR_RIGHT));
     img1  = lv_image_create(tile1);
-    lv_image_set_src(img1, DISP_FILE_MAP.at(evalkit::DISP444V1PV2));
+    lv_image_set_src(img1, DISP_FILE_MAP.at(evalkit::DisplayInfo::DISP444V1PV2));
     lv_obj_center(img1);
+    // add animation buttons
+    addAnimationButtons(tv, evalkit::DisplayInfo::DISP444V1PV2);
 
     bsp_display_unlock();
+    return ESP_OK;
+}
+
+esp_err_t Gui::addAnimationButtons(lv_obj_t* tv, evalkit::DisplayInfo::Ecd_e display)
+{
+    // add a new tile for animation buttons
+    lv_obj_t* tile1 = lv_tileview_add_tile(tv, 1, display, LV_DIR_LEFT);
+    // add anim buttons
+    lv_obj_t* list1 = lv_list_create(tile1);
+    lv_obj_set_size(list1, LV_PCT(100), LV_PCT(100));
+    for (const auto& btnName : evalkit::DisplayInfo::DISP_ANIM_NAMES[display])
+    {
+        lv_obj_t* btn = lv_list_add_btn(list1, NULL, btnName);
+    }
+
     return ESP_OK;
 }
 
